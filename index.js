7b223e9c1c5eaa759ad3e980d72155d4ad1be826
ibraheem <ibraheem45622@gmail.com>
2025-05-30 23:54:58 +0500

    cors issue

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 };
 
 // Middleware
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
